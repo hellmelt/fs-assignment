b2f97f205a65443f2cc1fb84757afebc759d6714
Anders Hjelm <2@andershjelm.com>
2018-04-23 00:15:10 +0200

    Played around with css to no avail

--- public/scripts/code.js
+++ public/scripts/code.js
@@ -1,25 +1,40 @@
 //
-//
+// Freespee frontend assignment.
+// Anders Hjelm, 2@andershjelm.com
+// 2018-04-23
+
+// ///////////////
+// Enclose everything in an IFFY, that is nice
 (() => {
+  // //////////
+  // Adds text to the #output	
   const appendOutput = (text) => {
   	document.querySelector('#output').value += text;
   };
 
+  // //////////
+  // Plays a DTMF sound, from the DTMF char
   const playSound = (text) => {
+  	if (text === '#') text = 'pound';
+  	if (text === '*') text = 'star';
   	const audio = document.querySelector('audio');
   	audio.src = '/sounds/' + text + '.wav';
   	audio.play();
   };
 
+  // //////////
+  // Sets upp a cell with the desired text and binds the click
   const setCell = (id, text) => {
   	const cell = document.querySelector('#' + id)
   	cell.innerText = text;
   	cell.addEventListener('click', () => {
   		appendOutput(text);
-  		playSound(text === '#' ? 'pound' : text);
+  		playSound(text);
   	});
   };
 
+  // //////////
+  // Calcute the sum of the digits in inputText. Beware of non-digits.
   const computeSum = (inputText) => {
     let acc = 0;
     const arr = [...inputText];
@@ -32,11 +47,13 @@
     return acc;
   };
 
+  // Set up the click on Sum botton. Grab the sum, type it in sumOutput
   document.querySelector('#sumButton').addEventListener('click', () => {
   	document.querySelector('#sumOutput').innerText = 'The sum is ' + 
   	computeSum(document.querySelector('#output').value);
   });
 
+  // Populate all the cells in the table
   setCell('c11', '1');
   setCell('c12', '2');
   setCell('c13', '3');
